3381aa5959fab89362b48813acd61c6d16379e4c
Giri Gimnastiar <girigimnastiarwork@gmail.com>
2022-09-23 10:22:40 +0700

    fixing clear

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,24 @@ const aboutH1 = document.querySelector("#about>h1");
 const imgAbout = document.querySelector("#about > aside > img");
 const skill = document.querySelector("#skills");
 const skillH1 = document.querySelector("#skills>h1");
+const asideDiv = document.querySelector("aside > div");
+const imgSkill = document.querySelector(".skillcss");
+const skillHtml = document.querySelector(".skillhtml");
+const skillJs = document.querySelector(".skilljavascript");
+const skillPhp = document.querySelector(".skillphp");
+const skillMysql = document.querySelector(".skillmysql");
+const assideH6 = document.querySelector(".inputan");
+const progressBar = document.querySelector("#field-progress-bar");
+const persentase = document.querySelector("#persentase");
+const ppAside = document.querySelector(".pp2");
+const figure = document.querySelector("figure");
+const cFigure = document.querySelector(".class-figure");
+const dropdown = document.querySelector(".class-figure > img");
+const sectionAside = document.querySelector("#section-aside");
+const instagramDevice = document.querySelector(".instagramDevice");
 const listSkill = document.querySelector(".list-skill");
-
+const listSkillDevice = document.querySelector(".list-skillDevice");
+const skillH1Device = document.querySelector(".skill-h1Device");
 var j = 0;
 var speed = 50;
 function getNama() {
@@ -74,19 +90,25 @@ btnWelcome.addEventListener("click", function () {
   linkedin.style.marginLeft = "10px";
   twitter.style.marginLeft = "10px";
   instagram.style.marginLeft = "10px";
+  instagramDevice.style.marginLeft = "-30px";
 });
 
 document.addEventListener("scroll", () => {
   var lastKnownScrollPosition = window.scrollY;
-  title.style.transform = "translate(" + lastKnownScrollPosition / 2 + "%,0px)";
+  title.style.transform = "translate(" + lastKnownScrollPosition / -0.5 + "%,0px)";
   pp.style.transform = "translate(0px," + -lastKnownScrollPosition + "%)";
   console.log(lastKnownScrollPosition);
   if (lastKnownScrollPosition > 10) {
+    body.style.overflowX = "hidden";
     aboutH1.style.marginLeft = "25%";
+    cFigure.style.display = "block";
+    cFigure.style.width = "240px";
     if (lastKnownScrollPosition > 200) {
       about.style.border = "2px solid rgb(240, 154, 210)";
       about.style.boxShadow = "0 0 10px rgb(52, 14, 38)";
       imgAbout.style.transform = "scale(200)";
+      cFigure.style.display = "block";
+      ppAside.style.marginTop = "-10px";
     }
   } else if (lastKnownScrollPosition < 10) {
     aboutH1.style.marginLeft = "-1000px";
@@ -94,22 +116,77 @@ document.addEventListener("scroll", () => {
       about.style.border = "2px solid white";
       about.style.boxShadow = "0 0 10px white";
       imgAbout.style.transform = "scale(0)";
+      cFigure.style.width = "10px";
+      cFigure.style.display = "block";
+      ppAside.style.marginTop = "-10px";
+      sectionAside.style.height = "0px";
+      sectionAside.style.display = "none";
+      dropdown.style.transform = "rotate(270deg)";
     }
   }
 
   if (lastKnownScrollPosition > 600) {
     skillH1.style.marginLeft = "25%";
+    skillH1Device.style.marginLeft = "32%";
     if (lastKnownScrollPosition > 600) {
+      asideDiv.style.marginLeft = "2260px";
       skill.style.border = "2px solid rgb(240, 154, 210)";
       skill.style.boxShadow = "0 0 10px rgb(52, 14, 38)";
       listSkill.style.marginLeft = "10px";
+      listSkillDevice.style.marginLeft = "50px";
     }
   } else if (lastKnownScrollPosition < 600) {
     skillH1.style.marginLeft = "-1000px";
     if (lastKnownScrollPosition < 500) {
+      asideDiv.style.marginLeft = "-1500px";
       skill.style.border = "2px solid white";
       skill.style.boxShadow = "0 0 10px white";
       listSkill.style.marginLeft = "-1000px";
     }
   }
+
+  imgSkill.addEventListener("mouseover", function () {
+    assideH6.innerHTML = "CSS";
+    progressBar.style.width = "30%";
+    persentase.innerHTML = "60%";
+  });
+  skillHtml.addEventListener("mouseover", function () {
+    assideH6.innerHTML = "HTML";
+    progressBar.style.width = "70%";
+    persentase.innerHTML = "100%";
+  });
+  skillJs.addEventListener("mouseover", function () {
+    assideH6.innerHTML = "JAVASCRIPT";
+    progressBar.style.width = "70%";
+    persentase.innerHTML = "100%";
+  });
+  skillPhp.addEventListener("mouseover", function () {
+    assideH6.innerHTML = "PHP";
+    progressBar.style.width = "60%";
+    persentase.innerHTML = "90%";
+  });
+  skillMysql.addEventListener("mouseover", function () {
+    assideH6.innerHTML = "MySQL";
+    progressBar.style.width = "25%";
+    persentase.innerHTML = "50%";
+  });
+  ppAside.addEventListener("mouseover", function () {
+    // figure.classList.add("class-figure");
+    ppAside.style.transform = "rotate(360deg)";
+    cFigure.style.width = "250px";
+  });
+  ppAside.addEventListener("click", function () {
+    ppAside.style.transform = "rotate(-360deg)";
+    cFigure.style.width = "10px";
+    cFigure.style.display = "none";
+    sectionAside.style.height = "0px";
+    ppAside.style.marginTop = "10px";
+    dropdown.style.transform = "rotate(270deg)";
+    sectionAside.style.display = "none";
+  });
+  dropdown.addEventListener("click", function () {
+    dropdown.style.transform = "rotate(360deg)";
+    sectionAside.style.display = "block";
+    sectionAside.style.height = "290px";
+  });
 });
